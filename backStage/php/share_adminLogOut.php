80db641a1a5ab55dacea31ccfbca4e0c870b0b1b
henry830122 <55733521+henry830122@users.noreply.github.com>
2021-03-08 22:03:05 +0800

    delete back

--- backStage/php/share_adminLogOut.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION["adminNO"]);
-  unset($_SESSION["adminId"]);
-  unset($_SESSION["adminPsw"]);
-  unset($_SESSION["adminName"]);
-?>
--- backStage/updateQuestStatus.php
+++
@@ -1,9 +0,0 @@
-  <?php
-    require_once('../connectdd103g3.php');
-    $sql="update `questionandlibrary` set quesStatus = :quesStatus where quesNo = :quesNo";
-    $member = $pdo->prepare($sql);
-    $member->bindValue(":quesStatus", $_GET["quesStatus"]);
-    $member->bindValue(":quesNo", $_GET["quesNo"]);
-    $member->execute();
-    header("location:question.php");
-  ?>
